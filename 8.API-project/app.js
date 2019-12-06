a466695f67f1df4f3c1ebd2a901897ba41d45a2c
ndishlevoy <nikolay.dishlevoy@dataart.com>
2019-12-06 14:57:12 +0200

    test

---
+++ 8.API-project/app.js
@@ -0,0 +1,14 @@
+
+
+const input = document.getElementById('searchUser');
+
+input.addEventListener('keyup',(e) =>{
+
+    const inputValue = e.target.value;
+    if (inputValue !==''){
+        console.log(inputValue);
+    }else{
+        
+    }
+   
+})
---
+++ 8.API-project/github.js
@@ -0,0 +1,7 @@
+
+class GitHub {
+    construcor() {
+        this.client_id = '3ab3ae3b2a9b93ea6687';
+        this.client_secret = 'f7da8d76bad272047db738322cd8863276524e4a';
+    }
+}
